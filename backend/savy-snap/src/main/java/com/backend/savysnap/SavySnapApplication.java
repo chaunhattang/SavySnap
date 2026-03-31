54d86503dd6a53589bfbadc8ecdaf91df48d6b93
chaunhattang <tang.chaunhat.cit22@eiu.edu.vn>
2026-03-31 09:15:59 +0700

    Init Backend
    Create a folder "backend" by Java SpringBoot.

---
+++ backend/savy-snap/src/main/java/com/backend/savysnap/SavySnapApplication.java
@@ -0,0 +1,13 @@
+package com.backend.savysnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavySnapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavySnapApplication.class, args);
+    }
+
+}
