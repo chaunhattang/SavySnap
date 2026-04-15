1b71027ef020af6d82f5dd8bc5ab411b9f528744
chaunhattang <tang.chaunhat.cit22@eiu.edu.vn>
2026-04-15 15:00:32 +0700

    Update init infor admin

--- backend/savy-snap/src/main/java/com/backend/savysnap/configuration/ApplicationInitConfig.java
+++ backend/savy-snap/src/main/java/com/backend/savysnap/configuration/ApplicationInitConfig.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,18 +16,27 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApplicationInitConfig implements ApplicationRunner {
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
 
+    @Value("${app.admin.username}")
+    String adminUsername;
+
+    @Value("${app.admin.password}")
+    String adminPassword;
+
+    @Value("${app.admin.email}")
+    String adminEmail;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (!userRepository.existsByUsername("admin")) {
+        if (!userRepository.existsByUsername(adminUsername)) {
             User admin = User.builder()
-                    .username("admin")
-                    .password(passwordEncoder.encode("admin"))
-                    .email("admin@gmail.com")
+                    .username(adminUsername)
+                    .password(passwordEncoder.encode(adminPassword))
+                    .email(adminEmail)
                     .role(RoleEnum.ADMIN)
                     .build();
 
